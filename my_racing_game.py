befb5d989d8351076c1a94438a13108e30446291
Maciej <sasiusz@gmail.com>
2019-12-11 09:23:44 +0100

    first version

---
+++ my_racing_game.py
@@ -0,0 +1,175 @@
+import pygame, sys
+from math import *
+from pygame.locals import *
+import random
+
+# window
+WIDTH, HEIGHT = 800, 800
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+# player_car=pygame.image.load('player_car.png').convert_alpha()
+pygame.font.init()
+pygame.display.set_caption('Drive your own FERRARI 2019')
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+pygame.draw.polygon(win, (90, 179, 0), [(0, 250), (0, 550), (350, 250)])
+pygame.draw.polygon(win, (90, 179, 0), [(800, 250), (800, 550), (450, 250)])
+
+pygame.draw.line(win, (255, 0, 0), (0, 550), (350, 250), 5)
+pygame.draw.line(win, (255, 0, 0), (800, 550), (450, 250), 5)
+
+pygame.draw.polygon(win, (50, 50, 255), [(0, 0), (0, 250), (800, 250), (800, 0)])
+pygame.draw.circle(win, (255, 255, 42), (690, 100), 30)
+# linie
+x = 0
+y = 375
+
+# win = background.convert()
+# other items
+tree1 = pygame.image.load('tree1.png')
+tree2 = pygame.image.load('tree2.png')
+tre = [tree1, tree2, tree1, tree2, tree1, tree2]
+
+
+def losujdrzewo(x):
+    if random.randint(0, 1) == 0:
+        tre[x] = tree1
+    else:
+        tre[x] = tree2
+
+
+cloud = pygame.image.load('cloud.png')
+cy = 0
+# loading car
+car = pygame.image.load('player_car.png').convert_alpha()
+car_l = pygame.image.load('player_car_l.png')
+car_r = pygame.image.load('player_car_r.png')
+
+car = pygame.transform.scale(car, (160, 82))
+car_l = pygame.transform.scale(car_l, (160, 82))
+car_r = pygame.transform.scale(car_r, (160, 82))
+
+car_actual = car
+
+
+def drawline(x):
+    pygame.draw.line(win, (255, 255, 255), (400, 250 + x / 1.1), (400, 255 + x * 1.2), (x // 25) + 1)
+
+
+h1 = random.randint(-30, 160)
+h2 = random.randint(-30, 160)
+h3 = random.randint(-30, 160)
+h4 = random.randint(-30, 160)
+
+
+a = 0.05
+# main loop
+def main():
+    global x, y, car_actual, cy, h1, h2, h3, h4, tre,a,color
+    clock = pygame.time.Clock()
+    clock.tick(50)
+    color = [255, 252, 42]
+    while x >= 0:
+        cy += 0.03
+        pygame.event.get()
+        keys = pygame.key.get_pressed()
+
+        #pygame.draw.circle(win, tuple(color), (690, 100), 30)
+
+        if color[1] > 253 or color[1] < 3:
+            a *= -1
+
+
+        color[1] -= a
+
+        # chmury
+        pygame.draw.polygon(win, (90, 179, 0), [(0, 250), (0, 550), (350, 250)])
+        pygame.draw.polygon(win, (90, 179, 0), [(800, 250), (800, 550), (450, 250)])
+
+        pygame.draw.polygon(win, (50, 50, 255), [(0, 0), (0, 250), (800, 250), (800, 0)])
+        pygame.draw.circle(win, color, (690, 100), 30)
+
+        if (cy % 1000) - 200 < -190:
+            h1 = random.randint(-30, 160)
+        if ((cy + 240) % 1000) - 200 < -190:
+            h2 = random.randint(-30, 160)
+        if ((cy + 600) % 1000) - 200 < -190:
+            h3 = random.randint(-30, 160)
+        if ((cy + 850) % 1000) - 200 < -190:
+            h4 = random.randint(-30, 160)
+
+        win.blit(cloud, ((cy % 1000) - 200, h1))
+        win.blit(cloud, (((cy + 240) % 1000) - 200, h2))
+        win.blit(cloud, (((cy + 600) % 1000) - 200, h3))
+        win.blit(cloud, (((cy + 850) % 1000) - 200, h4))
+
+        # drzewaprawo
+        if x % 1000 > 82:
+            tre1 = pygame.transform.scale(tre[0], (int((x % 1000) / 4.5), int((x % 1000) / 4.5)))
+            win.blit(tre1, (450 + (1.5 * (x % 1000)) / 4.5, 220 + int(0.7 * (x % 1000)) / 4.5))
+        elif int(x % 1000) == 1:
+            losujdrzewo(0)
+        if (x + 333) % 1000 > 82:
+            tre2 = pygame.transform.scale(tre[1], (int(((x + 333) % 1000) / 4.5), int(((x + 333) % 1000) / 4.5)))
+            win.blit(tre2, (450 + (1.5 * ((x + 333) % 1000)) / 4.5, 220 + int(0.7 * ((x + 333) % 1000)) / 4.5))
+        elif int((x + 333) % 1000) == 1:
+            losujdrzewo(1)
+        if (x + 666) % 1000 > 82:
+            tre3 = pygame.transform.scale(tre[2], (int(((x + 666) % 1000) / 4.5), int(((x + 666) % 1000) / 4.5)))
+            win.blit(tre3, (450 + (1.5 * ((x + 666) % 1000)) / 4.5, 220 + int(0.7 * ((x + 666) % 1000)) / 4.5))
+        elif int((x + 666) % 1000) == 1:
+            losujdrzewo(2)
+
+        # drzewalewo
+        if (x + 166) % 1000 > 82:
+            tre4 = pygame.transform.scale(tre[3], (int(((x + 166) % 1000) / 4.5), int(((x + 166) % 1000) / 4.5)))
+            win.blit(tre4, (320 - (2.5 * ((x + 166) % 1000)) / 4.5, 220 + int(0.7 * ((x + 166) % 1000)) / 4.5))
+        elif int((x + 166) % 1000) == 1:
+            losujdrzewo(3)
+        if (x + 500) % 1000 > 82:
+            tre5 = pygame.transform.scale(tre[4], (int(((x + 501) % 1000) / 4.5), int(((x + 500) % 1000) / 4.5)))
+            win.blit(tre5, (320 - (2.5 * ((x + 500) % 1000)) / 4.5, 220 + int(0.7 * ((x + 500) % 1000)) / 4.5))
+        elif int((x + 500) % 1000) == 1:
+            losujdrzewo(4)
+        if (x + 833) % 1000 > 82:
+            tre6 = pygame.transform.scale(tre[5], (int(((x + 833) % 1000) / 4.5), int(((x + 833) % 1000) / 4.5)))
+            win.blit(tre6, (320 - (2.5 * ((x + 833) % 1000)) / 4.5, 220 + int(0.7 * ((x + 833) % 1000)) / 4.5))
+        elif int((x + 833) % 1000) == 1:
+            losujdrzewo(5)
+
+        pygame.draw.polygon(win, (132, 132, 132), [(350, 250), (0, 550), (0, 700), (800, 700), (800, 551), (450, 250)])
+        drawline(int(x % 500))
+        drawline(int((x + 180) % 500))
+        drawline(int((x + 360) % 500))
+
+        # Movement controls
+        if keys[K_UP]:
+            x += 0.75
+            if keys[K_LEFT]:
+                if y > 35:
+                    y -= 0.2
+                car_actual = car_l
+            if keys[K_RIGHT]:
+                if y < 606:
+                    y += 0.2
+                car_actual = car_r
+        if keys[K_DOWN]:
+            x -= 0.3
+            if keys[K_LEFT]:
+                if y > 35:
+                    y -= 0.05
+                car_actual = car_r
+            if keys[K_RIGHT]:
+                if y < 606:
+                    y += 0.05
+                car_actual = car_l
+
+        # showing score
+        text = font.render('Score: ' + str(int(x)), True, (255, 0, 0))
+        win.blit(text, pygame.Rect(10, 220, 10, 10))
+
+        win.blit(car_actual, (y, 500))
+        pygame.display.update()
+        car_actual = car
+
+
+main()
---
+++ scoreboard.py
@@ -0,0 +1,6 @@
+
+
+
+def showscoreboard():
+    pass
+    #showing first 3 results, then sliding rest
